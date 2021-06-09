bd16b41c8e73d6ee1e5f47d1d73dc96b40135386
BRAD JONES <ttgbjo27@gmail.com>
2021-06-09 18:23:13 -0500

    added task 1

--- index.js
+++ index.js
@@ -45,9 +45,12 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
-}    
+function copy(){
+    const originalFlavors = [...originalFlavors];
+    return [...originalFlavors];
+}
+   
+    console.log('task 1', [...originalFlavors]);
 
 
 
