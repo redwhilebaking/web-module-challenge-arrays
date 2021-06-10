6d7a22f07390cdda86ec98304c88075bda48419f
BRAD JONES <ttgbjo27@gmail.com>
2021-06-09 19:24:38 -0500

    added task 4

--- index.js
+++ index.js
@@ -109,9 +109,11 @@ Use the removeLastFlavor function below to do the following:
     For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
-   /*your code here*/
+function removeLastFlavor(){
+    (originalFlavors.pop());
+    console.log('task 4',originalFlavors);
 }
+
 
 
 
